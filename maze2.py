ca97bbbd2b62ace1d862126b3f8e616fc4dcfe8a
Riley Moses <victi.resurgo@gmail.com>
2012-11-16 19:40:47 -0700

    predecessor is recorded incorrectly

--- maze2.py
+++ maze2.py
@@ -3,14 +3,43 @@
 import string
 from graph_tool.all import *
 
+# Filename for the final graph
+imageFile = "graph.png"
+# Vertext properties
 graph = Graph()
 vprop_label = graph.new_vertex_property("string")
 vprop_color = graph.new_vertex_property("string")
 vprop_type = graph.new_vertex_property("string")
 vprop_props = graph.new_vertex_property("object")
+# Eclude unneeded vertices and endges from final graph
+vprop_include = graph.new_vertex_property("bool")
+eprop_include = graph.new_edge_property("bool")
+
 vertices = []
 numVillages = 0
 numVertices = 0
+pred = []
+path = []
+
+# DFSVisitor class used with depth-first search
+class CustomDFSVisitor(DFSVisitor):
+	def __init__(self, name, pred):
+		self.name = name
+		self.pred = pred
+		self.end = False
+
+	def discover_vertex(self, u):
+		print "--> Traverse", self.name[u]
+		self.pred.append(u)
+		vprop_include[u] = True
+
+	def examine_edge(self, e):
+		eprop_include[e] = True
+
+	def finish_vertex(self, u):
+		# self.pred.append(u)
+		if vprop_props[u][4] == 70:
+			raise StopSearch
 
 def initializeFromFile(filename):
 	global numVillages
@@ -38,14 +67,16 @@ def buildGraph():
 	for vert in vertices:
 		i += 1
 		vert.append(i)
-		# Two vertices for each edge from the original map, one for traversing the edge in each direction
+		# Two vertices for each edge from the original map
+		# One for traversing the edge in alphabetical order
 		v = graph.add_vertex()
 		vprop_label[graph.vertex(int(v))] = (str(i) + vert[0] + vert[1])
 		vprop_color[graph.vertex(int(v))] = vert[2]
 		vprop_type[graph.vertex(int(v))] = vert[3]
 		vprop_props[graph.vertex(int(v))] = vert
+		vprop_include[graph.vertex(int(v))] = False
 		count = 0
-		
+		# And one for the opposite direction
 		v = graph.add_vertex()
 		vprop_label[graph.vertex(int(v))] = (str(i) + vert[1] + vert[0])
 		vprop_color[graph.vertex(int(v))] = vert[2]
@@ -57,19 +88,43 @@ def buildGraph():
 		reverseDir.append(vert[3])
 		reverseDir.append(vert[4])
 		vprop_props[graph.vertex(int(v))] = reverseDir
+		vprop_include[graph.vertex(int(v))] = False
 
+	# Connect vertices
 	for vSource in graph.vertices():
 		for vDest in graph.vertices():
 			if vprop_props[vSource][4] != vprop_props[vDest][4]:
 				if (vprop_props[vSource][1] == vprop_props[vDest][0]) and (vprop_props[vSource][2] == vprop_props[vDest][2] or vprop_props[vSource][3] == vprop_props[vDest][3]):
-					print "Link", vprop_label[vSource], "and", vprop_label[vDest]
-					graph.add_edge(vSource, vDest);
-		# print vprop_label[vSource], vprop_props[vSource]
-		
+					# print "Edge between", vprop_props[vSource], "and", vprop_props[vDest]
+					e = graph.add_edge(vSource, vDest);
+					eprop_include[e] = False
+		# print vprop_props[vSource]
 
+# Print the path of predecessors
+def printPath():
+	global pred
+	prev = ""
+	for p in pred:
+		print vprop_label[graph.vertex(p)]
+		# village = vprop_props[graph.vertex(p)][0]
+		# if prev != village:
+		# 	path.append(village)
+		# prev = village
+	path.append('j')
+	print "Path:",
+	for village in path:
+		print village,
+	print
 
 print "Loading vertices..."
 initializeFromFile("input.txt")
+print "Building graph..."
 buildGraph()
-print "Drawing..."
-graph_draw(graph, vertex_text=vprop_label, vertex_fill_color=vprop_color, vertex_font_size=14, output_size=(1000, 1000), output="graph.png")
+print "Searching..."
+dfs_search(graph, graph.vertex(0), CustomDFSVisitor(vprop_label, pred))
+print "Done!"
+printPath()
+print "Saving graph to \'" + imageFile + "\'..."
+# graph.set_edge_filter(eprop_include)
+# graph.set_vertex_filter(vprop_include);
+graph_draw(graph, vertex_text=vprop_label, vertex_fill_color=vprop_color, vertex_font_size=14, edge_color=[0.179, 0.203, 0.210, 1.0], output_size=(1500, 1500), output=imageFile)
